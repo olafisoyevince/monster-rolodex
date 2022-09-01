ce712f5adcb08f148532318816c5d2c0067efc27
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-09-01 11:14:16 +0100

    Added comments to explain the flow of the code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   constructor() {
     super();
 
+    // Step 1 - Initialize the default state
     this.state = {
       monsters: [],
       searchField: "",
@@ -17,6 +18,7 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
+        // Step 2 - Set the state to the initialized state created above
         this.setState(
           () => {
             return { monsters: users };
@@ -28,6 +30,8 @@ class App extends Component {
       );
   }
 
+  // Step 4 - In the onSearchChange call back function, event is passed into it as a
+  // parameter and fron there the value is passed into the searchField.
   onSearchChange = (event) => {
     const searchField = event.target.value.toLocaleLowerCase();
 
@@ -42,6 +46,8 @@ class App extends Component {
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
+    // Step 5 - The searchField is then used to filter the array the was fetched and stored in
+    // monsters state.
     const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
@@ -50,10 +56,12 @@ class App extends Component {
       <div className="App">
         <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
+          // Step 3 - Whatever is typed in the SearchBox component is captured in the SearchChange callback
           onChangeHandler={onSearchChange}
           placeholder="searchbox"
           className="monsters-search-box"
         />
+        {/* Step 6 - The filtered array is what is being used to create the card list array */}
         <Cardlist monsters={filteredMonsters} />
       </div>
     );
